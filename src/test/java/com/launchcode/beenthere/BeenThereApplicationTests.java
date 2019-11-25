6214982ae36f07be0304a72b90e582650afaa117
dearbournegal <dearbournegal@yahoo.com>
2019-11-25 14:14:21 -0500

    initial commit

---
+++ src/test/java/com/launchcode/beenthere/BeenThereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.beenthere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeenThereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
